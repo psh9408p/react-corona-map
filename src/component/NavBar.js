1b19298f7c229faa5955d46ff7becc37325b85ea
박성현 <parksh@bagseonghyeon-ui-MacBookPro.local>
2020-11-07 02:39:05 +0900

    second commit

---
+++ src/component/NavBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styled, { css } from "styled-components";
+
+const NavBarUI = styled.div`
+  width: 100%;
+  background-color: dodgerblue;
+`;
+
+const NavBar = () => {
+  return <NavBarUI>NavBar</NavBarUI>;
+};
+
+export default NavBar;
