ebfee9b74460582669376259bbb50bfdf1f1ccb0
박성현 <parksh@bagseonghyeon-ui-MacBookPro.local>
2020-11-07 22:43:46 +0900

    add myPosition button

--- src/component/InfoBox.js
+++ src/component/InfoBox.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled, { css } from "styled-components";
-
+const { kakao } = window;
 const InfoBoxUI = styled.div`
   position: absolute;
   top: 100px;
@@ -18,12 +18,58 @@ const InfoBoxTitle = styled.div`
   font-weight: bolder;
 `;
 
+const MyPosition = styled.div`
+  position: absolute;
+  top: 180px;
+  z-index: 10000;
+  background: white;
+  left: 20px;
+  padding: 15px;
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  border-radius: 4px;
+  text-align: center;
+  cursor: pointer;
+  font-weight: bolder;
+`;
+
 const InfoBox = () => {
   return (
-    <InfoBoxUI>
-      <InfoBoxTitle>현재날짜</InfoBoxTitle>
-      <div>2020.11.07</div>
-    </InfoBoxUI>
+    <>
+      <InfoBoxUI>
+        <InfoBoxTitle>현재날짜</InfoBoxTitle>
+        <div>2020.11.07</div>
+      </InfoBoxUI>
+      <MyPosition
+        onClick={() => {
+          if ("geolocation" in navigator) {
+            navigator.geolocation.getCurrentPosition((position) => {
+              const lat = position.coords.latitude;
+              const lng = position.coords.longitude;
+              const latlng = new kakao.maps.LatLng(lat, lng);
+
+              const InfoContainer = document.getElementById("myMap");
+              const InfoOptions = {
+                center: latlng,
+                level: 5,
+              };
+
+              const map = new kakao.maps.Map(InfoContainer, InfoOptions);
+
+              const InfoMarker = new kakao.maps.Marker({
+                map,
+                position: latlng,
+                // title: el.title,
+              });
+              InfoMarker.setMap(map);
+            });
+          } else {
+            alert("위치정보를 사용할 수 없습니다 !");
+          }
+        }}
+      >
+        현재위치
+      </MyPosition>
+    </>
   );
 };
 
